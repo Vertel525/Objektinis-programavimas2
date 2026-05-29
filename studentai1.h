ab5fd55a0163c5207cde98a10e61d0d114dd3bae
M.Vertelis <matas.vertelis@mif.stud.vu.lt>
2026-05-29 03:01:58 +0300

    Fix variable access in skaiciuoti function

--- studentai1.h
+++ studentai1.h
@@ -17,25 +17,26 @@ public:
     Studentas() : egz_(0), vid_(0.0), med_(0.0) {}
 };
 
+void skaiciuoti() {
+    int n = (int)paz_.size();
 
-    void skaiciuoti() {
-        int n = (int)paz.size();
-        if (n == 0) return;
+    if (n == 0) return;
+    int sum = 0;
 
-        int sum = 0;
-        for (int x : paz) sum += x;
-        vid = sum * 1.0 / n * 0.4 + egz * 0.6;
+    for (int x : paz_) sum += x;
+    vid_ = sum * 1.0 / n * 0.4 + egz_ * 0.6;
 
-        std::vector<int> sorted = paz;
-        std::sort(sorted.begin(), sorted.end());
+    std::vector<int> sorted = paz_;
+    std::sort(sorted.begin(), sorted.end());
 
-        if (n % 2 != 0)
-            med = sorted[n / 2];
-        else
-            med = (sorted[n / 2 - 1] + sorted[n / 2]) / 2.0;
-
-        med = med * 0.4 + egz * 0.6;
-    }
+    if (n % 2 != 0)
+        med_ = sorted[n / 2];
+    else
+        med_ = (sorted[n / 2 - 1] + sorted[n / 2]) / 2.0;
+    med_ = med_ * 0.4 + egz_ * 0.6;
+}
+    
+    
 
 
 #endif
